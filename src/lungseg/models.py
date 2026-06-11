274d9095a1a6960d630f10e720689ae76ac76437
Sara Rajabzadeh <>
2026-06-11 15:42:48 +0200

    Implement complete U-Net architecture from scratch

--- src/lungseg/models.py
+++ src/lungseg/models.py
@@ -21,9 +21,42 @@ class UNet(nn.Module):
     def __init__(self, in_channels=1, out_channels=1, features=[64, 128, 256, 512]):
         super().__init__()
         self.downs = nn.ModuleList()
+        self.ups = nn.ModuleList()
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        self.bottleneck = DoubleConv(features[-1], features[-1] * 2)
         
         in_ch = in_channels
         for feature in features:
             self.downs.append(DoubleConv(in_ch, feature))
             in_ch = feature
+            
+        for feature in reversed(features):
+            self.ups.append(
+                nn.ConvTranspose2d(feature * 2, feature, kernel_size=2, stride=2)
+            )
+            self.ups.append(DoubleConv(feature * 2, feature))
+        
+        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
+        
+    def forward(self, x):
+        skip_connections = []
+        for down in self.downs:
+            x = down(x)
+            skip_connections.append(x)
+            x = self.pool(x)
+        
+        x = self.bottleneck(x)
+        
+        skip_connections = skip_connections[::-1]
+        for idx in range(0, len(self.ups), 2):
+            x = self.ups[idx](x)
+            skip = skip_connections[idx // 2]
+        
+            if x.shape != skip.shape:
+                x = torch.nn.functional.interpolate(x, size=skip.shape[2:])
+            
+            x = torch.cat((skip, x), dim=1)
+            x = self.ups[idx + 1](x)
+        
+        return self.final_conv(x)
